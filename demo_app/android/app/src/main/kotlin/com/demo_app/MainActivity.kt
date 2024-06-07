a946eb089c1a016ac1f0e9d089376cf46433de52
Rasmus Wiuff <rwiuff@gmail.com>
2024-06-07 07:56:05 +0200

    cleanup

--- demo_app/android/app/src/main/kotlin/com/demo_app/MainActivity.kt
+++ demo_app/android/app/src/main/kotlin/com/demo_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.windows
+package com.demo_app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- demo_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ demo_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,40 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import audio_session
-import battery_plus
-import connectivity_plus
-import device_info_plus
-import flutter_appauth
-import flutter_local_notifications
-import flutter_secure_storage_macos
-import flutter_timezone
-import just_audio
-import location
-import oidc_macos
-import package_info_plus
-import path_provider_foundation
-import shared_preferences_foundation
-import sqflite
-import url_launcher_macos
-import window_to_front
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
-  BatteryPlusMacosPlugin.register(with: registry.registrar(forPlugin: "BatteryPlusMacosPlugin"))
-  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
-  FlutterAppauthPlugin.register(with: registry.registrar(forPlugin: "FlutterAppauthPlugin"))
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
-  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
-  FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
-  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
-  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
-  OidcPlugin.register(with: registry.registrar(forPlugin: "OidcPlugin"))
-  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  WindowToFrontPlugin.register(with: registry.registrar(forPlugin: "WindowToFrontPlugin"))
 }
