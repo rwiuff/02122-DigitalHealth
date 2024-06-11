7b0cfd61fb38532ab80dd6dc6e14ea48aba2aeb7
Niclas Juul Schæffer <Niclasschaeffer96@gmail.com>
2024-06-11 12:47:29 +0200

    some test but useless for now

---
+++ digital_health_module/test/test.dart
@@ -0,0 +1,38 @@
+library digital_health_module;
+import 'dart:js_interop_unsafe';
+
+import 'package:carp_mobile_sensing/carp_mobile_sensing.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:digital_health_module/main.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+
+void main() {
+  test('Initialize Module', () async {
+    await initializeModule();
+    
+  });
+
+  test('Set Study', () async {
+    int temp = SmartPhoneClientManager().studyCount;
+    await setStudy('test_study_id');
+    expect(SmartPhoneClientManager().studyCount, temp+1);
+  });
+
+  test('Start Study', () async {
+    await startStudy();
+    
+    
+  });
+
+  test('Stop Study', () async {
+    await stopStudy();
+    
+  });
+
+  test('Dispose Study', () async {
+    await disposeStudy();
+    
+  });
+}
