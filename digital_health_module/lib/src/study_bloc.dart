69d5daabea85bf87c741073488d6df44c315a42d
Rasmus Wiuff <rwiuff@gmail.com>
2024-06-20 15:04:31 +0200

    Stuff

--- digital_health_module/lib/src/study_bloc.dart
+++ digital_health_module/lib/src/study_bloc.dart
@@ -3,48 +3,56 @@ part of '../main.dart';
 class StudyBLoC {
   final phone = Smartphone();
   late SmartphoneStudyProtocol protocol;
+  final client = SmartPhoneClientManager();
   final healthService = HealthService();
+  late SmartphoneDeploymentController controller;
 
   Future<void> initialise() async {
     SamplingPackageRegistry().register(HealthSamplingPackage());
+    for (var package in SamplingPackageRegistry().packages) {
+      info('Registered package ${package.runtimeType}');
+    }
     protocol = SmartphoneStudyProtocol(
       name: 'Test Protocol',
       dataEndPoint: SQLiteDataEndPoint(),
     );
     protocol.addPrimaryDevice(phone);
     protocol.addConnectedDevice(healthService, phone);
-    await SmartPhoneClientManager().configure();
+    await client.configure();
     info('$runtimeType initialized');
   }
 
   Future<void> setStudy() async {
     protocol.addTaskControl(
-        DelayedTrigger(delay: const Duration(seconds: 10)),
+        PeriodicTrigger(period: const Duration(minutes: 2)),
         BackgroundTask(measures: [
           HealthSamplingPackage.getHealthMeasure([
             HealthDataType.STEPS,
           ])
         ]),
-        healthService);
-    SmartPhoneClientManager().addStudyProtocol(protocol);
+        healthService,
+        Control.Start);
+    Study study = await client.addStudyProtocol(protocol);
+    controller = client.getStudyRuntime(study)!;
     info('Study set');
   }
 
   Future<void> startStudy() async {
-    SmartPhoneClientManager().start();
+    HealthServiceManager().requestPermissions();
+    controller.start();
     info('Study started');
-    SmartPhoneClientManager()
+    controller
         .measurements
         .listen((measurement) => debugPrint(toJsonString(measurement)));
   }
 
   Future<void> stopStudy() async {
-    SmartPhoneClientManager().stop();
+    controller.stop();
     info('Study stopped');
   }
 
   Future<void> disposeStudy() async {
-    SmartPhoneClientManager().dispose();
+    controller.dispose();
     info('Study disposed');
   }
 }
